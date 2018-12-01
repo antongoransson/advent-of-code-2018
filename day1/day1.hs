5836b63bb3e2b90f5899868cbbb1d05a558e7751
antongoransson <anton.goransson95@gmail.com>
2018-12-01 15:21:30 +0100

    Update to use cycle

--- day1/day1.hs
+++ day1/day1.hs
@@ -4,7 +4,7 @@ import qualified Data.Set as Set
 main = do 
     input <- readLines "input.txt"
     putStrLn $ "Part 1: " ++ show (solvePart1 input)
-    putStrLn $ "Part 2: " ++ show (solvePart2 input 0 Set.empty 0)
+    putStrLn $ "Part 2: " ++ show (solvePart2 (cycle input) Set.empty 0)
 
 readLines = fmap (map (toInt . substitute '+' "") .  lines ) . readFile
 
@@ -19,7 +19,7 @@ substitute char sub = concatMap replace
 solvePart1 :: [Int] -> Int
 solvePart1 = sum
 
-solvePart2 :: [Int] -> Int -> Set Int -> Int -> Int
-solvePart2 freqs i visitedFreqs current
+solvePart2 :: [Int] -> Set Int -> Int -> Int
+solvePart2 (f:freqs) visitedFreqs current
     | current `Set.member` visitedFreqs = current
-    | otherwise = solvePart2 freqs ((i + 1) `mod` length freqs) (Set.insert current visitedFreqs) (current + freqs !! i)
+    | otherwise = solvePart2 freqs (Set.insert current visitedFreqs) (current + f)
--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,6 @@
 
+from itertools import cycle
+
 
 def solve_part_1(frequencies):
     return sum(frequencies)
@@ -9,17 +11,17 @@ def solve_part_2(frequencies):
     current_freq = 0
     unique_freq = set()
     l = len(frequencies)
-    while True:
-        current_freq += frequencies[i]
+    # while True:
+    for freq in cycle(frequencies):
+        current_freq += freq
         if current_freq in unique_freq:
             return current_freq
         unique_freq.add(current_freq)
-        i = (i + 1) % l
 
 
 def main():
     with open('input.txt') as f:
-        frequencies = [int(freq.replace('+', '')) for freq in f]
+        frequencies = [int(freq) for freq in f]
     sol1 = solve_part_1(frequencies)
     print('Part 1: {}'.format(sol1))
     sol2 = solve_part_2(frequencies)
