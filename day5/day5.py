d77613625df2aa52a018c6f43d1b99a4cd3de6cd
antongoransson <anton.goransson95@gmail.com>
2018-12-05 09:58:34 +0100

    Cleanup day5

--- day5/day5.py
+++ day5/day5.py
@@ -1,11 +1,10 @@
 def solve_part_1(string):
     s = list(string)
     i = 0
-    while i < len(s):
-        index = i % (len(s) - 1)
-        if ord(s[index]) == ord(s[index + 1]) - 32 or ord(s[index]) - 32 == ord(s[index + 1]):
-            s = s[0:index] + s[index + 2:]
-            i -= 1
+    while i < (len(s) - 1):
+        if ord(s[i]) == ord(s[i + 1]) - 32 or ord(s[i]) - 32 == ord(s[i + 1]):
+            s = s[0:i] + s[i + 2:]
+            i -= 1 if i > 0 else 0
         else:
             i += 1
     return len(s)
