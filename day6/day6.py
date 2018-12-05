141d341c7322311b0bdcc5a2764aba143659b42c
antongoransson <anton.goransson95@gmail.com>
2018-12-05 21:59:00 +0100

    Init commit day6

---
+++ day6/day6.py
@@ -0,0 +1,19 @@
+def solve_part_1(l):
+    pass
+
+
+def solve_part_2(l):
+    pass
+
+
+def main():
+    with open('input.txt') as f:
+        lines = [line for line in f]
+    sol1 = solve_part_1(lines)
+    print('Part 1: {}'.format(sol1))
+    sol2 = solve_part_2(lines)
+    print('Part 2: {}'.format(sol2))
+
+
+if __name__ == "__main__":
+    main()
