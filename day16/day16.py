23ab1e9a0a3347e149747c3a749b5e3233fba053
antongoransson <anton.goransson95@gmail.com>
2018-12-16 15:11:28 +0100

    Add day16 part2

--- day16/day16.py
+++ day16/day16.py
@@ -1,23 +1,3 @@
-opcodes = {
-    0: lambda regs, a, b, c: addr(regs, a, b, c),
-    1: lambda regs, a, b, c: addi(regs, a, b, c),
-    2: lambda regs, a, b, c: mulr(regs, a, b, c),
-    3: lambda regs, a, b, c: muli(regs, a, b, c),
-    4: lambda regs, a, b, c: banr(regs, a, b, c),
-    5: lambda regs, a, b, c: bani(regs, a, b, c),
-    6: lambda regs, a, b, c: borr(regs, a, b, c),
-    7: lambda regs, a, b, c: bori(regs, a, b, c),
-    8: lambda regs, a, b, c: setr(regs, a, b, c),
-    9: lambda regs, a, b, c: seti(regs, a, b, c),
-    10: lambda regs, a, b, c: gtir(regs, a, b, c),
-    11: lambda regs, a, b, c: gtri(regs, a, b, c),
-    12: lambda regs, a, b, c: gtrr(regs, a, b, c),
-    13: lambda regs, a, b, c: eqir(regs, a, b, c),
-    14: lambda regs, a, b, c: eqri(regs, a, b, c),
-    15: lambda regs, a, b, c: eqrr(regs, a, b, c)
-}
-
-
 def addr(regs, a, b, c):
     regs[c] = regs[a] + regs[b]
     return regs
@@ -98,33 +78,53 @@ def eqrr(regs, a, b, c):
     return regs
 
 
+opcodes = {
+    0: addr,
+    1: addi,
+    2: mulr,
+    3: muli,
+    4: banr,
+    5: bani,
+    6: borr,
+    7: bori,
+    8: setr,
+    9: seti,
+    10: gtir,
+    11: gtri,
+    12: gtrr,
+    13: eqir,
+    14: eqri,
+    15: eqrr
+}
+
+
 def solve_part_1(samples):
     tot = 0
     instructions = {}
-
-    # while len(instructions) < 15:
-    for sample in samples:
-        before, instruction, after = sample
-        before = list(before)
-        after = list(after)
-        instruction = list(instruction)
-        correct = []
-        for i in range(16):
-            regs = before[:]
-            # if instruction[0] not in instructions:
-            a = opcodes[i](regs, instruction[1],
-                           instruction[2], instruction[3])
-            if a == after:
-                correct.append(i)
-        if len(correct) >= 3:
-            tot += 1
-        elif len(correct) == 1:
-            instructions[instruction[0]] = correct[0]
+    while len(instructions) < 15:
+        for sample in samples:
+            before, instruction, after = sample
+            possible_instructions = []
+            correct = []
+            for i in range(16):
+                a = opcodes[i](before[:], *instruction[1:])
+                if instruction[0] not in instructions and i not in instructions.values():
+                    if a == after:
+                        possible_instructions.append(i)
+                if a == after:
+                    correct.append(i)
+            if len(correct) >= 3:
+                tot += 1
+            if len(possible_instructions) == 1:
+                instructions[instruction[0]] = possible_instructions[0]
     return tot, instructions
 
 
-def solve_part_2(score):
-    pass
+def solve_part_2(test_program, instructions):
+    regs = [0] * 4
+    for command in test_program:
+        opcodes[instructions[command[0]]](regs, *command[1:])
+    return regs
 
 
 def main():
@@ -132,14 +132,17 @@ def main():
     with open('input.txt') as f:
         part1, part2 = f.read().split('\n\n\n')
         for x in part1.split('\n\n'):
-            # print(x, '\n')
             before, instruction, after = x.split('\n')
             samples.append(
-                (map(int, before.split(': ')[1][1:-1].split(',')), map(int, instruction.split(' ')), map(int, after.split(':  ')[1][1:-1].split(','))))
-    sol1 = solve_part_1(samples)
+                (before[8:][1:-1].split(','), instruction.split(' '), after[8:][1:-1].split(',')))
+        test_program = [list(map(int, s.split(' ')))
+                        for s in part2.split('\n') if s]
+    samples = [(list(map(int, s[0])), list(map(int, s[1])),
+                list(map(int, s[2]))) for s in samples]
+    sol1, instructions = solve_part_1(samples)
     print('Part 1: {}'.format(sol1))
-    # sol2 = solve_part_2(score)
-    # print('Part 2: {}'.format(sol2))
+    sol2 = solve_part_2(test_program, instructions)
+    print('Part 2: {}'.format(sol2))
 
 
 if __name__ == "__main__":
